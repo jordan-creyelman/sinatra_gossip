2c208b541c5e037c9b130cc5dc68a53402a64b3f
jordan-creyelman <creyelmanjo@gmail.com>
2021-10-25 19:49:50 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# sinatra_gossip
+# sinatra_gossip
+# sinatra_gossip
+# sinatra_gossip
---
+++ lib/controller.rb
@@ -0,0 +1,26 @@
+require_relative 'gossip'
+
+class ApplicationController < Sinatra::Base
+
+  get '/' do
+    erb:index, locals: {gossips: Gossip.all}
+  end
+ 
+  get '/gossips/new/' do
+    erb:new_gossip
+  end 
+
+   post '/gossips/new/' do
+    puts "Ceci est le contenu du hash params : #{params}"
+    Gossip.new(params["gossip_author"], params["gossip_content"]).save
+    redirect '/'
+   end
+
+  
+  get '/gossips/:id' do 
+    erb:show, locals: {gossip: Gossip.find(params['id']), id: params['id']}
+  end
+
+
+
+end
