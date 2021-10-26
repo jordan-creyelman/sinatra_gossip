c32e548c9b1d62b729135db20f6223ebe3eb33dc
jordan-creyelman <creyelmanjo@gmail.com>
2021-10-26 08:21:26 +0200

    first commit

--- lib/controller.rb
+++ lib/controller.rb
@@ -6,21 +6,29 @@ class ApplicationController < Sinatra::Base
     erb:index, locals: {gossips: Gossip.all}
   end
  
-  get '/gossips/new/' do
+  get '/gossips/new' do
     erb:new_gossip
   end 
 
-   post '/gossips/new/' do
+   post '/gossips/new' do
     puts "Ceci est le contenu du hash params : #{params}"
     Gossip.new(params["gossip_author"], params["gossip_content"]).save
     redirect '/'
    end
-
-  
-  get '/gossips/:id' do 
+   get '/gossips/:id' do 
     erb:show, locals: {gossip: Gossip.find(params['id']), id: params['id']}
   end
   
+   get '/gossips/:id/edit' do
+    erb :edit, locals: {gossip: Gossip.all[params[:id].to_i ], id: params[:id].to_i}
+  end
+
+  #sauvegarde et met a jour le fichier csv avec les modifications
+  post '/gossips/:id/edit' do
+		Gossip.upgrade(params["gossip_author"], params["gossip_content"], params[:id].to_i)
+		redirect '/'
+  end
 
 
+  
 end
